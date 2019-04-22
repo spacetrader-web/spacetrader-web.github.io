28380fdbeb302b9646c5b1685a444af4ebe9e514
Rithik Gavvala <gavvalarithik@gmail.com>
2019-04-21 21:51:38 -0400

    form posting working

--- app/app.py
+++ app/app.py
@@ -2,24 +2,58 @@ import pymongo
 from flask import Flask, request, jsonify, json, render_template, flash, redirect, url_for
 from pymongo import MongoClient
 # from app import app
-from app.player_form import Player
+from player_form import Player
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any secret string'
+client = MongoClient("mongodb://admin:space123@ds145356.mlab.com:45356/spacetrader")
+db = client["spacetrader"]
+db.authenticate("admin","space123");
 
-client = pymongo.MongoClient("mongodb://admin:st@spacetrader-cluster-shard-00-00-yik2d.gcp.mongodb.net:27017,spacetrader-cluster-shard-00-01-yik2d.gcp.mongodb.net:27017,spacetrader-cluster-shard-00-02-yik2d.gcp.mongodb.net:27017/test?ssl=true&replicaSet=spacetrader-cluster-shard-0&authSource=admin&retryWrites=true")
-db = client.test
-print(db)
+
+
+print(db.users.find_one({}))
 
 
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """Login"""
     form = Player()
-    if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.playerName.data, form.pilotSkill.data, form.traderSkill.data, form.engineerSkill.data, form.fighterSkill.data))
-        return redirect(url_for('base'))
-    return render_template('base.html',  title='Sign In', form=form)
+    # print(form)
+    # # if form.validate_on_submit():
+    # if request.method == 'POST':
+    #     if form.validate():
+    #         flash('Login requested for user {}'.format(
+    #             form.playerName.data, form.pilotSkill.data, form.traderSkill.data, form.engineerSkill.data, form.fighterSkill.data))
+    #         db.insert_one ({
+    #             '_id' : 1,
+    #             'name': form.playerName.data,
+    #             'pilot': form.pilotSkill.data,
+    #             'trader': form.traderSkill.data,
+    #             'engineer': form.engineerSkill.data,
+    #             'fighter': form.fighterSkill.data
+    #         })
+    #         print("player insert")
+    #         return render_template('main.html', form = form)
+    # print(db)
+    #dump()
+    return render_template('base.html', form = form )
+
+
+    # return render_template('base.html',  title='Sign In', form=form)
+@app.route('/add_player', methods=['POST'])
+def add_player():
+    print(request.form.to_dict())
+    db.users.insert_one(request.form.to_dict())
+    return "Success!"
+    #dump()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#rahulrajus
+#teamformation
--- app/player_form.py
+++ app/player_form.py
@@ -1,14 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, SubmitField
+from wtforms import StringField, PasswordField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 
 
 class Player(FlaskForm):
-	playerName = StringField('Player Name', validators=[DataRequired()])
+	playerName = StringField('Player Name', validators=[DataRequired(message=('Player Name'))])
 	pilotSkill = IntegerField('Pilot Skill', validators=[DataRequired()])
 	traderSkill = IntegerField('Trader Skill', validators=[DataRequired()])
 	engineerSkill = IntegerField('Engineer Skill', validators=[DataRequired()])
 	fighterSkill = IntegerField('Fighter Skill', validators=[DataRequired()])
-	difficulty = StringField('Difficulty', validators=[DataRequired()])
-	submit = SubmitField('Sign In')
+	difficulty = SelectField('Difficulty',validators=[('1', 'Easy'), ('2', 'Medium'), ('3', 'Hard')])
+	submit = SubmitField('Submit')
+	#
+	# def validateSkills(form, pilotSkill, traderSkill, engineerSkill, fighterSkill):
+	# 	sum = pilotSkill.data + traderSkill.data + engineerSkill.data + fighterSkill.data
+	# 	if sum != 16:
+	# 		raise ValidationError("Skills must be equal to 16")
